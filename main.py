f8a69fbf361cf9343a06f176706a792dce59c177
Keith <robert.keith.holler@gmail.com>
2022-02-04 12:49:55 -0600

    Completed Game

--- main.py
+++ main.py
@@ -17,15 +17,13 @@ if random.randint(0, 1) == 0:
     else:
         destroyerRow2 = destroyerRow+1
         destroyerCol2 = string.ascii_lowercase[destroyerColumn]
+else:
     if not destroyerRow > 24:
         destroyerRow2 = destroyerRow+1
         destroyerCol2 = string.ascii_lowercase[destroyerColumn]
     else:
         destroyerRow2 = destroyerRow
         destroyerCol2 = string.ascii_lowercase[destroyerColumn + 1]
-else:
-    destroyerRow2 = destroyerRow+1
-    destroyerCol2 = string.ascii_lowercase[destroyerColumn]
 destroyer = [[destroyerRow, string.ascii_lowercase[destroyerColumn]], [
     destroyerRow2, destroyerCol2]]
 
@@ -47,6 +45,7 @@ if random.randint(0, 1) == 0:
         submarineRow3 = submarineRow+2
         submarineCol2 = string.ascii_lowercase[submarineColumn]
         submarineCol3 = string.ascii_lowercase[submarineColumn]
+else:
 
     if not submarineRow > 23:
         submarineRow2 = submarineRow+1
@@ -58,11 +57,7 @@ if random.randint(0, 1) == 0:
         submarineRow3 = submarineRow
         submarineCol2 = string.ascii_lowercase[submarineColumn + 1]
         submarineCol3 = string.ascii_lowercase[submarineColumn + 2]
-else:
-    submarineRow2 = submarineRow+1
-    submarineRow3 = submarineRow+2
-    submarineCol2 = string.ascii_lowercase[submarineColumn]
-    submarineCol3 = string.ascii_lowercase[submarineColumn]
+
 submarine = [[submarineRow, string.ascii_lowercase[submarineColumn]], [
     submarineRow2, submarineCol2], [submarineRow3, submarineCol3]]
 
@@ -84,6 +79,7 @@ if random.randint(0, 1) == 0:
         cruiserRow3 = cruiserRow+2
         cruiserCol2 = string.ascii_lowercase[cruiserColumn]
         cruiserCol3 = string.ascii_lowercase[cruiserColumn]
+else:        
     if not cruiserRow > 23:
         cruiserRow2 = cruiserRow+1
         cruiserRow3 = cruiserRow+2
@@ -95,11 +91,6 @@ if random.randint(0, 1) == 0:
         cruiserCol2 = string.ascii_lowercase[cruiserColumn + 1]
         cruiserCol3 = string.ascii_lowercase[cruiserColumn + 2]
 
-else:
-    cruiserRow2 = cruiserRow+1
-    cruiserRow3 = cruiserRow+2
-    cruiserCol2 = string.ascii_lowercase[cruiserColumn]
-    cruiserCol3 = string.ascii_lowercase[cruiserColumn]
 cruiser = [[cruiserRow, string.ascii_lowercase[cruiserColumn]],
            [cruiserRow2, cruiserCol2], [cruiserRow3, cruiserCol3]]
 
@@ -125,6 +116,7 @@ if random.randint(0, 1) == 0:
         battleshipCol2 = string.ascii_lowercase[battleshipColumn]
         battleshipCol3 = string.ascii_lowercase[battleshipColumn]
         battleshipCol4 = string.ascii_lowercase[battleshipColumn]
+else:
     if not battleshipRow > 22:
         battleshipRow2 = battleshipRow+1
         battleshipRow3 = battleshipRow+2
@@ -139,13 +131,7 @@ if random.randint(0, 1) == 0:
         battleshipCol2 = string.ascii_lowercase[battleshipColumn + 1]
         battleshipCol3 = string.ascii_lowercase[battleshipColumn + 2]
         battleshipCol4 = string.ascii_lowercase[battleshipColumn + 3]
-else:
-    battleshipRow2 = battleshipRow+1
-    battleshipRow3 = battleshipRow+2
-    battleshipRow4 = battleshipRow+3
-    battleshipCol2 = string.ascii_lowercase[battleshipColumn]
-    battleshipCol3 = string.ascii_lowercase[battleshipColumn]
-    battleshipCol4 = string.ascii_lowercase[battleshipColumn]
+
 battleship = [[battleshipRow, string.ascii_lowercase[battleshipColumn]],
               [battleshipRow2, battleshipCol2], [
                   battleshipRow3, battleshipCol3],
@@ -168,6 +154,7 @@ if random.randint(0, 1) == 0:
         carrierCol3 = string.ascii_lowercase[carrierColumn + 2]
         carrierCol4 = string.ascii_lowercase[carrierColumn + 3]
         carrierCol5 = string.ascii_lowercase[carrierColumn + 4]
+ 
     else:
         carrierRow2 = carrierRow+1
         carrierRow3 = carrierRow+2
@@ -177,6 +164,8 @@ if random.randint(0, 1) == 0:
         carrierCol3 = string.ascii_lowercase[carrierColumn]
         carrierCol4 = string.ascii_lowercase[carrierColumn]
         carrierCol5 = string.ascii_lowercase[carrierColumn]
+
+else:
     if not carrierRow > 21:
         carrierRow2 = carrierRow+1
         carrierRow3 = carrierRow+2
@@ -186,6 +175,7 @@ if random.randint(0, 1) == 0:
         carrierCol3 = string.ascii_lowercase[carrierColumn]
         carrierCol4 = string.ascii_lowercase[carrierColumn]
         carrierCol5 = string.ascii_lowercase[carrierColumn]
+
     else:
         carrierRow2 = carrierRow
         carrierRow3 = carrierRow
@@ -195,15 +185,6 @@ if random.randint(0, 1) == 0:
         carrierCol3 = string.ascii_lowercase[carrierColumn + 2]
         carrierCol4 = string.ascii_lowercase[carrierColumn + 3]
         carrierCol5 = string.ascii_lowercase[carrierColumn + 4]
-else:
-    carrierRow2 = carrierRow+1
-    carrierRow3 = carrierRow+2
-    carrierRow4 = carrierRow+3
-    carrierRow5 = carrierRow+4
-    carrierCol2 = string.ascii_lowercase[carrierColumn]
-    carrierCol3 = string.ascii_lowercase[carrierColumn]
-    carrierCol4 = string.ascii_lowercase[carrierColumn]
-    carrierCol5 = string.ascii_lowercase[carrierColumn]
 
 carrier = [[carrierRow, string.ascii_lowercase[carrierColumn]],
            [carrierRow2, carrierCol2], [carrierRow3, carrierCol3],
@@ -233,14 +214,14 @@ battle.index += 1
 
 
 shipsSunk=[]
-turns=100
+turns=50
 Welcome=text2art("WELCOME   TO   BATTLESHIP !!!")
 Win=text2art("YOU   WIN !!!")
 Lose=text2art("YOU LOSE !!!")
 print(Welcome)
-print('You have 100 turns to sink the fleet!')
+print(f'You have {turns} turns to sink the fleet!')
 while True: 
-    if turns == 100:
+    if turns == 50:
         input("Press Enter to start...")
         print(battle)
     # print(ships)
@@ -254,7 +235,7 @@ while True:
         except ValueError:
             print('Please enter an integer between 1 and 25')
         else:
-            if rowInput < 1: 
+            if rowInput < 1 or rowInput > 25: 
                 print('Out of Range')
                 continue
             else:
@@ -262,7 +243,7 @@ while True:
     while True:
 
         colInput = input("Choose a column letter: ")
-        if colInput.isalpha():
+        if colInput.isalpha() and len(colInput) == 1:
             break
         else:
             print('Please enter a letter')
@@ -271,48 +252,54 @@ while True:
 
 
     if rowInput == destroyer[0][0] and colInput == destroyer[0][1] or rowInput == destroyer[1][0] and colInput == destroyer[1][1]:
-        battle.at[int(rowInput), colInput.upper()] = 'DES'
+        battle.at[int(rowInput), colInput.upper()] = 'DE'
         print(battle)
+        print('You hit the Destroyer!!!')
     elif rowInput == submarine[0][0] and colInput == submarine[0][1] or rowInput == submarine[1][0] and colInput == submarine[1][1] or rowInput == submarine[2][0] and colInput == submarine[2][1]:
-        battle.at[int(rowInput), colInput.upper()] = 'SUB'
+        battle.at[int(rowInput), colInput.upper()] = 'SU'
         print(battle)
+        print('You hit the Submarine!!!')
     elif rowInput == cruiser[0][0] and colInput == cruiser[0][1] or rowInput == cruiser[1][0] and colInput == cruiser[1][1] or rowInput == cruiser[2][0] and colInput == cruiser[2][1]:
-        battle.at[int(rowInput), colInput.upper()] = 'CRU'
+        battle.at[int(rowInput), colInput.upper()] = 'CR'
         print(battle)
+        print('You hit the Cruiser!!!')
     elif rowInput == battleship[0][0] and colInput == battleship[0][1] or rowInput == battleship[1][0] and colInput == battleship[1][1] or rowInput == battleship[2][0] and colInput == battleship[2][1] or rowInput == battleship[3][0] and colInput == battleship[3][1]:
-        battle.at[int(rowInput), colInput.upper()] = 'BAT'
+        battle.at[int(rowInput), colInput.upper()] = 'BA'
         print(battle)
+        print('You hit the Battleship!!!')
     elif rowInput == carrier[0][0] and colInput == carrier[0][1] or rowInput == carrier[1][0] and colInput == carrier[1][1] or rowInput == carrier[2][
             0] and colInput == carrier[2][1] or rowInput == carrier[3][0] and colInput == carrier[3][1] or rowInput == carrier[4][0] and colInput == carrier[4][1]:
-        battle.at[int(rowInput), colInput.upper()] = 'CAR'
+        battle.at[int(rowInput), colInput.upper()] = 'CA'
         print(battle)
+        print('You hit the Carrier!!!')
     else:
         battle.at[rowInput, colInput.upper()] = ' '
         print(battle)
+        print("You Missed!!!")
 
 
-    if battle.at[destroyer[0][0],str(destroyer[0][1]).upper()] == 'DES' and battle.at[destroyer[1][0],str(destroyer[1][1]).upper()] == 'DES' :
+    if battle.at[destroyer[0][0],str(destroyer[0][1]).upper()] == 'DE' and battle.at[destroyer[1][0],str(destroyer[1][1]).upper()] == 'DE' :
         if 'Destroyer' not in shipsSunk:
             print("You Sunk the Destroyer!")
             shipsSunk.append('Destroyer')
 
 
-    if battle.at[submarine[0][0],str(submarine[0][1] ).upper()] == 'SUB' and battle.at[submarine[1][0] ,str(submarine[1][1]).upper()] == 'SUB' and battle.at[submarine[2][0]  ,str(submarine[2][1]).upper()] == 'SUB' :
+    if battle.at[submarine[0][0],str(submarine[0][1] ).upper()] == 'SU' and battle.at[submarine[1][0] ,str(submarine[1][1]).upper()] == 'SU' and battle.at[submarine[2][0]  ,str(submarine[2][1]).upper()] == 'SU' :
         if 'Submarine' not in shipsSunk:
             print("You Sunk the Submarine!")
             shipsSunk.append('Submarine')
 
-    if battle.at[cruiser[0][0],str(cruiser[0][1] ).upper()] == 'CRU' and battle.at[cruiser[1][0] ,str(cruiser[1][1]).upper()] == 'CRU' and battle.at[cruiser[2][0]  ,str(cruiser[2][1]).upper()] == 'CRU' :
+    if battle.at[cruiser[0][0],str(cruiser[0][1] ).upper()] == 'CR' and battle.at[cruiser[1][0] ,str(cruiser[1][1]).upper()] == 'CR' and battle.at[cruiser[2][0]  ,str(cruiser[2][1]).upper()] == 'CR' :
             if 'Cruiser' not in shipsSunk:
                 print("You Sunk the Cruiser!")
                 shipsSunk.append('Cruiser')
 
-    if battle.at[battleship[0][0],str(battleship[0][1] ).upper()] == 'BAT' and battle.at[battleship[1][0] ,str(battleship[1][1]).upper()] == 'BAT' and battle.at[battleship[2][0]  ,str(battleship[2][1]).upper()] == 'BAT' and battle.at[battleship[3][0]  ,str(battleship[3][1]).upper()] == 'BAT' :
+    if battle.at[battleship[0][0],str(battleship[0][1] ).upper()] == 'BA' and battle.at[battleship[1][0] ,str(battleship[1][1]).upper()] == 'BA' and battle.at[battleship[2][0]  ,str(battleship[2][1]).upper()] == 'BA' and battle.at[battleship[3][0]  ,str(battleship[3][1]).upper()] == 'BA' :
             if 'Battleship' not in shipsSunk:
                 print("You Sunk the Battleship!")
                 shipsSunk.append('Battleship')  
 
-    if battle.at[carrier[0][0],str(carrier[0][1] ).upper()] == 'BAT' and battle.at[carrier[1][0] ,str(carrier[1][1]).upper()] == 'BAT' and battle.at[carrier[2][0]  ,str(carrier[2][1]).upper()] == 'BAT' and battle.at[carrier[3][0]  ,str(carrier[3][1]).upper()] == 'BAT'  and battle.at[carrier[4][0]  ,str(carrier[4][1]).upper()] == 'BAT' :
+    if battle.at[carrier[0][0],str(carrier[0][1] ).upper()] == 'CA' and battle.at[carrier[1][0] ,str(carrier[1][1]).upper()] == 'CA' and battle.at[carrier[2][0]  ,str(carrier[2][1]).upper()] == 'CA' and battle.at[carrier[3][0]  ,str(carrier[3][1]).upper()] == 'CA'  and battle.at[carrier[4][0]  ,str(carrier[4][1]).upper()] == 'CA' :
             if 'Carrier' not in shipsSunk:
                 print("You Sunk the Carrier!")
                 shipsSunk.append('Carrier')
